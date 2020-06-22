a31c656cd038940edb4c6a3b3bd1bd0a1e59aea9
VietAnh Nguyen <vietanhnguyen31@gmail.com>
2020-06-22 16:32:08 +0700

    Init project

---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Todo List goes here
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoList'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import TodoList from '@/components/TodoList'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/todo',
+      name: 'todolist',
+      component: TodoList
+    }
+  ]
+})
