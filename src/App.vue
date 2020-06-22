0fc88222055c5b46c8a8ed2acd27015ae1c09e32
vietanh-dev <vietanhnguyen31@gmail.com>
2020-06-22 22:31:33 +0700

    create Todo List model

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="container">
+    <img class="logo" src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,6 +12,23 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  border: none;
+  outline: none;
+}
+li {
+  list-style: none;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+.logo {
+  width: 60px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,65 @@
 <template>
     <div>
         Todo List goes here
+        <div class="todo-app">
+            <input type="text" class="todo-input" name="" v-model="todoItem" @keyup.enter="addTodo" >
+            <ul>
+                <li v-for="(todo, i) in todoList" :key="i">
+                    <input type="checkbox" name="todo[]" v-bind:id="'todoItem_' + todo.id">
+                    <label v-bind:for="'todoItem_' + todo.id">{{ todo.title }}</label>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'TodoList'
+    name: 'TodoList',
+    data() {
+        return {
+            todoItem: "",
+            todoList: [{
+                title: "Finish toto list by Vuejs",
+                completed: false,
+                id: 0
+            }]
+        }
+    },
+    methods: {
+        addTodo() {
+            if (this.todoItem.trim().length == 0) {
+                return
+            }
+            this.todoList.push({
+                title: this.todoItem,
+                completed: false,
+                id: this.todoList.length
+            })
+            this.todoItem = ""
+        }
+    }
 }
 </script>
 
-<style scoped>
-
+<style lang="postcss">
+.todo-app {
+    text-align: left;
+}
+input[type=text], .form-control {
+    width: 100%;
+    padding: 10px 15px;
+    border: 1px solid lightblue;
+    lineheight: 17px;
+}
+.todo-input {
+    margin: 20px auto;
+}
+.todo-app ul li {
+    margin: 10px auto;
+}
+.todo-app input[type=checkbox] {
+    display: inline-block;
+    margin-right: 10px;
+}
 </style>
