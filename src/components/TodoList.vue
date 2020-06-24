5758537d56ededfc7d9aee7bdddc1dd2f322eeb4
vietanh-dev <vietanhnguyen31@gmail.com>
2020-06-24 23:30:53 +0700

    Make todo editable

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,10 +4,14 @@
         <div class="todo-app">
             <input type="text" class="todo-input" name="" v-model="todoItem" @keyup.enter="addTodo" placeholder="What needs to be done">
             <ul>
-                <li v-for="(todo, index) in todoList" :key="index">
-                    <input type="checkbox" name="todo[]" v-bind:id="'todoItem_' + todo.id">
-                    <label v-bind:for="'todoItem_' + todo.id">{{ todo.title }}</label>
-                    <span class="close-button" @click="removeTodo(index)">&times;</span>
+                <li v-for="(todo, i) in todoList" :key="i">
+                    <input type="checkbox" name="todo[]" v-bind:id="'todoItem_' + todo.id" v-model="todo.completed">
+                    <div class="todo-title-wrapper">
+                        <label v-bind:for="'todoItem_' + todo.id" v-if="!todo.editing" @dblclick="editTodo(todo)" :class="{ checked: todo.completed }">{{ todo.title }}</label>
+                        <input type="text" class="form-control" v-model="todo.title" v-else @blur="doneEdit(todo)" v-focus 
+                        @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)">
+                    </div>
+                    <span class="close-button" @click="removeTodo(i)">&times;</span>
                 </li>
             </ul>
         </div>
@@ -23,8 +27,10 @@ export default {
             todoList: [{
                 title: "Finish toto list by Vuejs",
                 completed: false,
-                id: 0
-            }]
+                id: 0,
+                editing: false,
+            }],
+            beforeEditCache: ''
         }
     },
     methods: {
@@ -35,18 +41,42 @@ export default {
             this.todoList.push({
                 title: this.todoItem,
                 completed: false,
-                id: this.todoList.length
+                id: this.todoList.length,
+                editing: false,
             })
             this.todoItem = ""
         },
         removeTodo(index) {
             this.todoList.splice(index, 1)
+        },
+        editTodo(el) {
+            this.beforeEditCache = el.title
+            el.editing = true
+        },
+        doneEdit(el) {
+            if (el.title.trim() == '') {
+                el.title = this.beforeEditCache
+            }
+            el.editing = false
+        },
+        cancelEdit(el) {
+            el.title = this.beforeEditCache
+            el.editing = false
+        }
+    },
+    directives: {
+        focus: {
+            inserted: function(el) {
+                el.focus()
+            }
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+$input_fontsize: 18px;
+
 .todo-app {
     text-align: left;
 }
@@ -58,15 +88,33 @@ input[type=text], .form-control {
 }
 .todo-input {
     margin: 20px auto;
-    font-size: 18px;
+    font-size: $input_fontsize;
 }
 .todo-app {
     ul li {
-        margin: 10px auto;
+        margin: 3px auto 0;
+        min-height: 45px;
+    }
+    li {
+        display: flex;
+        align-items: center;
     }
     input[type=checkbox] {
         display: inline-block;
         margin-right: 10px;
+    }
+    .todo-title-wrapper {
+        flex-grow: 1;
+        .form-control {
+            font-size: $input_fontsize;
+        }
+        label {
+            display: block;
+            &.checked {
+                text-decoration: line-through;
+                color: grey;
+            }
+        }
     }
     .close-button {
         float: right;
